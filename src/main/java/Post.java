1585b7d5536567898a8a91424ead17663676a4bc
Anastasya Biruk <anastasya.biruk7@gmail.com>
2024-06-08 23:58:30 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; // Имя
+    public String surname; // Фамилия
+    public String patronymic; // Отчество
+    public String phone; // Телефон
+    public String passport; // Паспорт
+    public boolean subscription; // Подписка
+    public FormDate birthday; // День рождения
+}
+
